95cf650024610d66348674ca497b46a606d17187
fairydust <rtyh12@gmail.com>
2023-11-02 17:22:42 +0200

    Fix player vertical and horizontal motion at the same time

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -2,10 +2,21 @@ extends Node
 
 
 @export var physics_body: Node2D
-@export var speed: float
+@export var collision_shape: CollisionShape2D
+@export var run_speed: float
+
+var speed_vertical = 0
 
 func _physics_process(delta):
+	speed_vertical -= 1 * delta
+	
+	var pos_offset_this_frame = Vector2.ZERO
+	
+	pos_offset_this_frame.y += speed_vertical * delta
+	
 	if Input.is_action_pressed("ui_right"):
-		physics_body.position.x += speed
+		pos_offset_this_frame.x += run_speed * delta
 	if Input.is_action_pressed("ui_left"):
-		physics_body.position.x -= speed
+		pos_offset_this_frame.x -= run_speed * delta
+		
+	physics_body.position += pos_offset_this_frame	
