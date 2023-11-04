d8cf6c8424a5f0a2a0761ec8ff9fb2b982f3152d
fairydust <rtyh12@gmail.com>
2023-11-04 23:46:00 +0200

    Use move_and_slide()

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -1,27 +1,29 @@
 extends Node
 
 
-@export var physics_body: CharacterBody2D
+@export var cb: CharacterBody2D
 @export var collision_shape: CollisionShape2D
 @export var run_speed: float
+
 
 var speed_vertical = 0
 
 func _physics_process(delta):
+	cb.velocity = Vector2.ZERO
+	
 	# Gravity
-	speed_vertical -= -1 * delta
+#	speed_vertical -= -1 * delta
 	
-	var frame_mov = Vector2.ZERO
-	
-	frame_mov.y += speed_vertical * delta
+	cb.velocity.y += speed_vertical
 	
 	# Collisions
-	print(physics_body.is_on_floor())
+#	print(cb.is_on_floor())
 	
 	# Input
 	if Input.is_action_pressed("ui_right"):
-		frame_mov.x += run_speed * delta
+		cb.velocity.x += run_speed
 	if Input.is_action_pressed("ui_left"):
-		frame_mov.x -= run_speed * delta
+		cb.velocity.x -= run_speed
 		
-	physics_body.position += frame_mov	
+	print(cb.velocity)
+	cb.move_and_slide()	
